2b276ccf74d26870bd9f4e3703de5ff011c76dbc
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2024-09-18 18:20:50 +0200

    feat: Fetch and save all user files

--- src/index.js
+++ src/index.js
@@ -20,12 +20,43 @@ const requestInterceptor = new RequestInterceptor([
     method: 'GET',
     url: '/moyenscontactindividu/etatcivil',
     serialization: 'json'
+  },
+  {
+    identifier: 'userMessages',
+    method: 'GET',
+    url: '/documents-usager/v2/courriers?nbElements',
+    serialization: 'json'
   }
 ])
 requestInterceptor.init()
 
-const baseUrl = 'https://www.francetravail.fr/accueil/'
+const PDF_HEADERS = {
+  Accept: 'application/json, text/plain, */*',
+  'Content-Type': 'application/pdf'
+}
+const ATTESTATION_HEADERS = {
+  Accept:
+    'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/png,image/svg+xml,*/*;q=0.8',
+  'Accept-Encoding': 'gzip, deflate, br, zstd',
+  Referer:
+    'https://candidat.francetravail.fr/candidat/situationadministrative/suiviinscription/attestation/recapitulatif',
+  Host: 'candidat.francetravail.fr',
+  'Accept-Language': 'fr-FR,fr;q=0.8,en-US;q=0.5,en;q=0.3',
+  Connection: 'keep-alive',
+  'Upgrade-Insecure-Requests': 1,
+  'Sec-Fetch-Dest': 'document',
+  'Sec-Fetch-Mode': 'navigate',
+  'Sec-Fetch-Site': 'same-origin',
+  'Sec-Fetch-User': '?1',
+  Priority: 'u=0, i'
+}
+
+// const baseUrl = 'https://www.francetravail.fr/accueil/'
 const loginFormUrl = 'https://candidat.francetravail.fr/espacepersonnel/'
+const attestationsPageUrl =
+  'https://candidat.pole-emploi.fr/candidat/situationadministrative/suiviinscription/attestation/mesattestations/true'
+const courriersPageUrl =
+  'https://authentification-candidat.pole-emploi.fr/compte/redirigervers?url=https://courriers.pole-emploi.fr/courriersweb/acces/AccesCourriers'
 
 class PoleemploiContentScript extends ContentScript {
   async onWorkerReady() {
@@ -59,9 +90,12 @@ class PoleemploiContentScript extends ContentScript {
       this.store.userCredentials = { login, password }
     }
     if (event === 'requestResponse') {
-      const { identifier, response } = payload
+      const { identifier } = payload
       this.log('debug', `${identifier} request intercepted`)
-      this.store[identifier] = { response }
+      this.store[identifier] = { payload }
+      if (identifier === 'userMessages') {
+        this.store.token = payload.requestHeaders.Authorization
+      }
     }
   }
 
@@ -107,6 +141,22 @@ class PoleemploiContentScript extends ContentScript {
     if (!authenticated) {
       return true
     }
+    await this.clickAndWait(
+      '#step1-candidat > div > button',
+      'button[data-target="#PopinDeconnexion"]'
+    )
+    await this.clickAndWait(
+      'button[data-target="#PopinDeconnexion"]',
+      '.modal-footer'
+    )
+    await this.runInWorker('click', 'button[data-target="#PopinDeconnexion"]')
+    await this.waitForElementInWorker('button', {
+      includesText: 'Quitter mon espace'
+    })
+    await this.runInWorker('click', 'button', {
+      includesText: 'Quitter mon espace'
+    })
+    await this.waitForElementInWorker('#keywords-selectized')
     return true
   }
 
@@ -119,10 +169,9 @@ class PoleemploiContentScript extends ContentScript {
   async checkAuthenticated() {
     // Logout button does not exists until the menu has been clicked
     // So to check authentication, we're waiting for all major elements to be completly loaded
-    const messagesElement = document.querySelector('#messages')
     const notificationsElement = document.querySelector('#notifications')
     const projectElement = document.querySelector('#step4')
-    return Boolean(messagesElement && notificationsElement && projectElement)
+    return Boolean(notificationsElement && projectElement)
   }
 
   async showLoginFormAndWaitForAuthentication() {
@@ -153,13 +202,132 @@ class PoleemploiContentScript extends ContentScript {
     if (this.store.userCredentials) {
       await this.saveCredentials(this.store.userCredentials)
     }
+    const courriers = await this.fetchMessages()
+    await this.saveFiles(courriers, {
+      context,
+      fileIdAttributes: ['vendorRef'],
+      contentType: 'application/pdf',
+      qualificationLabel: 'unemployment_benefit'
+    })
+    const attestations = await this.fetchAttestations()
+    await this.saveFiles([attestations], {
+      context,
+      fileIdAttributes: ['vendorRef'],
+      contentType: 'application/pdf',
+      qualificationLabel: 'employment_center_certificate'
+    })
     await this.waitForElementInWorker('[pause]')
     const identity = await this.runInWorker('parseIdentity')
     await this.saveIdentity(identity)
   }
+
+  async fetchMessages() {
+    this.log('info', '📍️ fetchMessages starts')
+    await this.goto(courriersPageUrl)
+    await this.waitForElementInWorker('.courriers')
+    const interceptedMessages = this.store.userMessages.payload.response
+    const computedMessages = await this.computeMessages(
+      interceptedMessages.ressources
+    )
+    return computedMessages
+  }
+
+  async computeMessages(messages) {
+    this.log('info', '📍️ computeMessages starts')
+    const computedMessages = []
+    for (const message of messages) {
+      const dateArray = message.dateDiffusion.match(/(.{4})(.{2})(.{2})/)
+      const date = `${dateArray[1]}-${dateArray[2]}-${dateArray[3]}`
+      const type = message.lmod
+      const courriersUrl =
+        'https://api.pole-emploi.fr/documents-usager/v2/courriers/'
+      const vendorRef = message.idDn
+      const fileurl = `${courriersUrl}${vendorRef}`
+
+      const computedMessage = {
+        date,
+        type,
+        fileurl,
+        vendorRef,
+        filename: `${date}_polemploi_${type}_${vendorRef}.pdf`,
+        vendor: 'Pole Emploi',
+        fileAttributes: {
+          metadata: {
+            contentAuthor: 'pole-emploi.fr',
+            carbonCopy: true,
+            issueDate: new Date(date)
+          }
+        },
+        requestOptions: {
+          headers: {
+            Authorization: this.store.token,
+            ...PDF_HEADERS
+          }
+        }
+      }
+      computedMessages.push(computedMessage)
+    }
+    return computedMessages
+  }
+
+  async fetchAttestations() {
+    this.log('info', '📍️ fetchAttestations starts')
+    await this.goto(attestationsPageUrl)
+    await this.waitForElementInWorker('#attestationsSelectModel')
+    await this.evaluateInWorker(() => {
+      document.querySelector('#attestationsSelectModel').value =
+        'AVIS_DE_SITUATION'
+    })
+    await this.clickAndWait('#valider', '#telechargerAttestationPdf')
+    const attestation = await this.runInWorker('computeAttestation')
+    return attestation
+  }
+
+  async computeAttestation() {
+    this.log('info', '📍️ computeAttestation starts')
+    const blob = await ky
+      .get(
+        'https://candidat.francetravail.fr/candidat/situationadministrative/suiviinscription/attestation/recapitulatif:telechargerattestationpdf',
+        {
+          headers: {
+            ...ATTESTATION_HEADERS
+          }
+        }
+      )
+      .blob()
+    const dataUri = await blobToBase64(blob)
+    const today = new Date()
+    const formattedDate = formatDate(today)
+    const situationReport = {
+      date: new Date(),
+      shouldReplaceFile: () => true,
+      filename: `${formattedDate}_polemploi_Dernier avis de situation.pdf`,
+      dataUri,
+      vendorRef: 'AVIS_DE_SITUATION',
+      vendor: 'Pole Emploi',
+      fileAttributes: {
+        metadata: {
+          contentAuthor: 'pole-emploi.fr',
+          carbonCopy: true,
+          issueDate: new Date()
+        }
+      }
+    }
+    return situationReport
+  }
 }
 
 const connector = new PoleemploiContentScript({ requestInterceptor })
-connector.init({ additionalExposedMethodsNames: [] }).catch(err => {
-  log.warn(err)
-})
+connector
+  .init({ additionalExposedMethodsNames: ['computeAttestation'] })
+  .catch(err => {
+    log.warn(err)
+  })
+
+function formatDate(date) {
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+
+  return `${year}-${month}-${day}`
+}
